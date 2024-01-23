90157a647b3c7fd84f328fe49fccf67e6968decf
Lenghak Hok <125646707+Lenghak@users.noreply.github.com>
2024-01-23 19:21:37 +0700

    added weather exercise

---
+++ weather/weather.go
@@ -0,0 +1,14 @@
+// Package weather contains the forecasting package of the current weather condition of various cities in Goblinocus.
+package weather
+
+// CurrentCondition represents the current weather condition.
+var CurrentCondition string
+
+// CurrentLocation represetns the current corresponding location with the current weather condition.
+var CurrentLocation string
+
+// Forecast returns a string contains the current location correspond with the current weather condition.
+func Forecast(city, condition string) string {
+	CurrentLocation, CurrentCondition = city, condition
+	return CurrentLocation + " - current weather condition: " + CurrentCondition
+}
