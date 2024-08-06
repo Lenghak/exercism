c70555841a5c99b8ede712ecd9be4eb502b37364
Lenghak Hok <lenghakhok.dev@gmail.com>
2024-08-06 17:06:33 +0700

    +meteorology.go v0.0.1

--- main.go
+++ main.go
@@ -1,16 +1,11 @@
 package main
 
 import (
-	parsinglogfiles "exercism/parsing_log_files"
+	"exercism/meteorology"
 	"fmt"
 )
 
 func main() {
 
-	fmt.Println(parsinglogfiles.TagWithUserName([]string{
-		"[WRN] User James123 has exceeded storage space.\n",
-		"[WRN] Host down. User   Michelle4 lost connection.\n",
-		"[INF] Users can login again after 23:00. \n",
-		"[DBG] We need to check that user names are at least 6 chars long. \n",
-	}))
+	fmt.Println(meteorology.Celsius)
 }
